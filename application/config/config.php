44ea91cdcc885269a5b84fadc160e51ee4df2a55
Khairul Azam <skyview059@gmail.com>
2016-09-19 00:54:15 +0600

    Use Composer Activated  - used package class.upload.php

--- application/config/config.php
+++ application/config/config.php
@@ -137,8 +137,8 @@ $config['subclass_prefix'] = 'MY_';
 | Note: This will NOT disable or override the CodeIgniter-specific
 |	autoloading (application/config/autoload.php)
  */
-$config['composer_autoload'] = false;
-// $config['composer_autoload'] = '../vendor/autoload.php';
+//$config['composer_autoload'] = false;
+$config['composer_autoload'] = '/vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
